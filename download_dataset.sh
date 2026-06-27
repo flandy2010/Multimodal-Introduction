c475f4df8415b1380fbc1583d784a6da557e4cbe
wuxiuyu.wxy <xiuyuwu@tencent.com>
2026-06-27 20:26:40 +0800

    shell:更新了数据集下载脚本，新增bonsai下载路径

--- download_dataset.sh
+++ download_dataset.sh
@@ -2,3 +2,6 @@ cd data
 mkdir 360_extra_scenes && cd 360_extra_scenes
 wget https://storage.googleapis.com/gresearch/refraw360/360_extra_scenes.zip
 unzip 360_extra_scenes.zip
+
+wget https://storage.googleapis.com/gresearch/refraw360/bonsai.zip
+unzip bonsai.zip
